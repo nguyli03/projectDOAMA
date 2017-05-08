1c14022a3ccf36f9c20895a03348a446630ce68e
Linh Nguyen Le <nguyli03@.luther.edu>
2017-05-08 11:54:17 -0500

    initial commit

---
+++ myserver.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, Response
+from json import dumps, loads
+from flask_pymongo import PyMongo
+from flask import jsonify
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+import psycopg2
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+@app.route("/")
+def index.html():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
